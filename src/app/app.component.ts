60f1ad774849af81cbc1161e1b9c366bb5372846
Diana Laxmytkina <dlaxmytkina@mail.ru>
2022-03-29 10:47:25 +0300

    adaptive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,46 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { ChildrenOutletContexts, NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { slideRightAnimation, triggerRouteAnimation } from './animations'; 
+import { slideRightAnimation, triggerRouteAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [triggerRouteAnimation, slideRightAnimation]
+  animations: [triggerRouteAnimation,
+    trigger("openclose_menu", [
+
+      transition(":enter", [
+        style({ transform: "translateY(-100%)" }),
+        animate("400ms ease-out", style({ transform: "translateY(0)" }))
+      ]),
+      transition(":leave", [
+        style({ transform: "translateY(0)" }),
+        animate("400ms ease-out", style({ transform: "translateY(-100%)" }))
+      ])
+
+    ])
+  ]
 })
 export class AppComponent {
 
-  constructor(private contexts: ChildrenOutletContexts, private router: Router) { 
-      this.router.events.subscribe((event) => {
-        if (event instanceof NavigationEnd && event.id>1) {
-          window.scrollTo({
-            top: 0,
-            behavior: "smooth"
+  constructor(private contexts: ChildrenOutletContexts, private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd && event.id > 1) {
+        this.open_menu();
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
         });
-        }
-      })
+      }
+    })
   }
-
+  menu_open = false;
+  open_menu() {
+    this.menu_open = this.menu_open === false ? true : false
+  }
   getRouteAnimationData() {
-   return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
   }
   goHome() {
     this.router.navigate([''])
